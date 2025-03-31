1e452e73d07ac3034469056c4353553f77e0ed0a
Pedro Salvarezza <pedrosalvarezza7@gmail.com>
2025-03-31 11:52:43 -0300

    "Actualización del mensaje de bienvenida".

--- LaboratorioProgramacion/Program.cs
+++ LaboratorioProgramacion/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("¡Lindo Lunes para estar con Visual Studio 2022!");
+Console.WriteLine("Cambios en git");
